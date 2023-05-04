e26e15dce3354d435b629dcd05809e206185b995
ramsurat0880 <surajkumar231501@gmail.com>
2023-05-04 21:37:48 +0530

    day-2 backend task

---
+++ backend/Middleware/Authorization.js
@@ -0,0 +1,20 @@
+const jwt = require("jsonwebtoken");
+const authorization = (req, res, next) => {
+  const token = req.headers.authorization;
+  if (token) {
+    const decoded = jwt.verify(token, process.env.SECRET_KEY);
+    if (decoded.userRole === "admin") {
+      next();
+    } else {
+      res
+        .status(401)
+        .send({ msg: "You are not authorised to do this action!!", ok: false });
+    }
+  } else {
+    res
+      .status(401)
+      .send({ msg: "You are not authorised to do this action!!", ok: false });
+  }
+};
+
+module.exports = {authorization}
