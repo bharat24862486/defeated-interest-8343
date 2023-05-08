a9c138aa9923cd20305a34d45494a4b6f8b4abbd
vinaykumar7580 <vinayhatwar16@gmail.com>
2023-05-08 21:51:30 +0530

    navbar added on product page

--- frontend/src/redux/store.js
+++ frontend/src/redux/store.js
@@ -1,9 +1,11 @@
 import { applyMiddleware, combineReducers, legacy_createStore } from "redux";
 import thunk from "redux-thunk";
 import AdminReducer from "./admin/admin.reducer";
+import {reducer as ProductReducer} from "../redux/ProductReducer/reducer"
 
 const rootReducer = combineReducers({
-  AdminReducer,
+  ProductReducer,
+  AdminReducer
 });
 
 export const store = legacy_createStore(rootReducer, applyMiddleware(thunk));
--- frontend/src/routes/MainRoutes.jsx
+++ frontend/src/routes/MainRoutes.jsx
@@ -1,5 +1,4 @@
 import { Routes, Route } from "react-router-dom";
-import Product from "../pages/Product";
 import SingleProduct from "../pages/SingleProduct";
 import CartPage from "../pages/CartPage";
 import PaymentPage from "../pages/PaymentPage";
@@ -10,12 +9,15 @@ import AdminProducts from "../admin/AdminProducts";
 import PrivateRoute from "../admin/Components/PrivateRoute";
 import Settings from "../admin/Settings";
 import Home from "../pages/Home";
+import ProductData from "../pages/ProductData";
+import Product from "../pages/Product"
+
 
 function MainRoutes() {
   return (
     <Routes>
       <Route path="/" element={<Home />} />
-      <Route path="/product" element={<Product />} />
+      <Route path="/product" element={<Product/>} />
       <Route path="/product/:id" element={<SingleProduct />} />
       <Route path="/cart" element={<CartPage />} />
       <Route path="/payment" element={<PaymentPage />} />
